a4938dbdb88e7b0bb8a1b07376253f6d4659f1aa
Acumen <zkraft@ACUMEN.local>
2016-10-26 11:29:20 -0500

    sample code
    OOP sample code.

---
+++ user.php
@@ -0,0 +1,26 @@
+<?php
+
+class User
+{
+/*********************Construct()******************************/
+	function __construct()
+    {
+    	//parent::__construct();
+    }
+
+ 	/**
+ 	 * @author                          Zachary Kraft
+ 	 * Purpose of the function          Description
+ 	 * @variable 						: type
+ 	 * @return 							return_type 
+ 	 */
+ 	public function funName()
+ 	{
+ 		
+ 	}
+ 	/*---------------End of funName()---------------*/
+
+    
+}// End User --------------Class{}
+//Owner : Zachary Kraft
+  ?>
